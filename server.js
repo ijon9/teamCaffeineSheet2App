5aa073fc055a4da606a9b1d96e4056a6b6eb8370
ijon9 <isaac.jon72@gmail.com>
2023-03-14 18:45:27 -0400

    google authentication

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express = require("express")
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    // res.send('Hello World!')
-    res.sendFile(path.join(__dirname, '/pages/index.html'));
+    // res.sendFile(path.join(__dirname, '/pages/index2.html'))
+    res.sendFile(path.join(__dirname, '/pages/index.html'))
 })
 
-app.get('/home', (req, res) => {
+app.post('/home', (req, res) => {  
     res.sendFile(path.join(__dirname, '/pages/home.html'))
 })
 
